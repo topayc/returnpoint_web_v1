ccabea91c1487bc457c3db230342575f30525c54
topayc <topayc@naver.com>
2019-02-19 20:36:23 +0900

    ....

--- src/main/webapp/resources/js/lib/common.js
+++ src/main/webapp/resources/js/lib/common.js
@@ -152,7 +152,7 @@ var host = window.location.protocol + "//" + window.location.host;
 var appInfo = {
 	//appSchem : "intent://returnp?seq=67#Intent;scheme=rp;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.returnp.app;end",
 	appSchem : "rp://returnp",
-	appPackage : "com.returnp.returnpointapp",
+	appPackage : "com.tophappyworld.returnpointapp",
 	access : access,
 	share : {
 		title : '리턴포인트.',
